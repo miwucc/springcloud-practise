f3c47de4ed7a7736b24e4669884a7b9796dc7619
xxlforest <xxlforest@qq.com>
2019-05-10 23:44:32 +0800

    1

--- spring-cloud-eureka/spring-cloud-eureka/src/main/java/com/neo/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 独立启动一个eureka服务
+ */
 @SpringBootApplication
+//作为服务器启动
 @EnableEurekaServer
 public class SpringCloudEurekaApplication {
-	//测试上传
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaApplication.class, args);
 	}
