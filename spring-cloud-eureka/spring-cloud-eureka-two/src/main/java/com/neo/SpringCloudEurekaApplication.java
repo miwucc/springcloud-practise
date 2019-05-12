1aafb23b7d17b85853788097834867678dfa5799
xxlforest <xxlforest@qq.com>
2019-05-12 22:29:45 +0800

    1

--- spring-cloud-eureka/spring-cloud-eureka-two/src/main/java/com/neo/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka-two/src/main/java/com/neo/SpringCloudEurekaApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 独立启动2个eureka服务互相作为客户端注册到对方，组成高可用
+ * 启动的时候请先启动peer1环境，再启动peer2环境
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringCloudEurekaApplication {
